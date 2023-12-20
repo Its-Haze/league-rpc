c0a5c0ed5d0e99e6b1e024e15fa11ec8a9a3fb3e
Haze <haze.gpg@proton.me>
2023-12-20 04:28:23 +0100

    Remove Riot service check, and refactor code.

--- league_rpc_linux/__main__.py
+++ league_rpc_linux/__main__.py
@@ -11,64 +11,32 @@ from league_rpc_linux.kda import get_kda
 from league_rpc_linux.processes.process import (
     check_discord_process,
     check_league_client_process,
-    check_riot_games_service_process,
     player_state,
 )
+from league_rpc_linux.reconnect import discord_reconnect_attempt
 
 # Discord Application: League of Linux
 DEFAULT_CLIENT_ID = "1185274747836174377"
 DISCORD_PROCESS_NAMES = ["Discord", "DiscordPTB", "DiscordCanary", "electron"]
+LEAGUE_OF_LEGENDS_LOGO = "https://github.com/Its-Haze/league-rpc-linux/blob/master/assets/leagueoflegends.png?raw=true"
+SMALL_TEXT = "github.com/Its-Haze/league-rpc-linux"
 
 
-parser = argparse.ArgumentParser(description="Script with Discord RPC.")
-parser.add_argument(
-    "--client-id",
-    type=str,
-    default=DEFAULT_CLIENT_ID,
-    help="Client ID for Discord RPC. Default is 1185274747836174377. which will show 'League of Linux' on discord",
-)
-parser.add_argument(
-    "--no-stats",
-    action="store_true",
-    help="use '--no-stats' to Opt out of showing in-game stats (KDA, minions) in Discord RPC",
-)
-parser.add_argument(
-    "--add-process",
-    nargs="+",
-    default=[],
-    help="Add custom Discord process names to the search list.",
-)
-
-
-args = parser.parse_args()
-
-
-def main():
+def main(cli_args: argparse.Namespace):
     """
-    main program.
+    This is the program that gets executed.
     """
-    print(Colors().logo)
-
-    if args.no_stats:
-        print(
-            f"{Colors.green}Argument --no-stats detected.. Will {Colors.red}not {Colors.green}show InGame stats{Colors.reset}"
-        )
-    if args.add_process:
-        print(
-            f"{Colors.green}Argument --add-process detected.. Will add {Colors.blue}{args.add_process}{Colors.green} to the list of Discord processes to look for.{Colors.reset}"
-        )
-
+    ############################################################
+    ## Check Discord, RiotClient & LeagueClient processes     ##
     rpc = check_discord_process(
-        process_names=DISCORD_PROCESS_NAMES + args.add_process, client_id=args.client_id
+        process_names=DISCORD_PROCESS_NAMES + cli_args.add_process,
+        client_id=cli_args.client_id,
     )
 
-    check_riot_games_service_process()
     check_league_client_process()
-
-    print(f"{Colors.green}\nRich Presence utilized!{Colors.reset}")
-    league_of_legends_logo = "https://freepngimg.com/save/85643-blue-league-legends-icons-of-symbol-garena/1600x1600.png"
+    print(f"\n{Colors.green}Successfully connected to Discord RPC!{Colors.reset}")
+    ############################################################
     start_time = int(time.time())
-
     while True:
         try:
             match player_state():
@@ -93,9 +61,9 @@ def main():
                                 large_image=skin_asset,
                                 large_text=champ_name,
                                 details=gamemode,
-                                state=f"In Game {get_kda() if not args.no_stats else ''}",
-                                small_image=league_of_legends_logo,
-                                small_text="github.com/Its-Haze/league-rpc-linux",
+                                state=f"In Game {get_kda() if not cli_args.no_stats else ''}",
+                                small_image=LEAGUE_OF_LEGENDS_LOGO,
+                                small_text=SMALL_TEXT,
                                 start=int(time.time())
                                 - get_current_ingame_time(default_time=start_time),
                             )
@@ -120,8 +88,8 @@ def main():
                                     large_text="Playing TFT",
                                     details="Teamfight Tactics",
                                     state=f"In Game · lvl: {level}",
-                                    small_image=league_of_legends_logo,
-                                    small_text="github.com/Its-Haze/league-rpc-linux",
+                                    small_image=LEAGUE_OF_LEGENDS_LOGO,
+                                    small_text=SMALL_TEXT,
                                     start=int(time.time())
                                     - get_current_ingame_time(default_time=start_time),
                                 )
@@ -129,7 +97,7 @@ def main():
 
                 case "InLobby":
                     rpc.update(  # type:ignore
-                        large_image=league_of_legends_logo,
+                        large_image=LEAGUE_OF_LEGENDS_LOGO,
                         large_text="github.com/Its-Haze/league-rpc-linux",
                         state="In Client",
                         start=start_time,
@@ -149,30 +117,45 @@ def main():
             print(
                 f"{Colors.red}Discord seems to be closed, will attempt to reconnect!{Colors.reset}"
             )
-            amount_of_tries = 10
-            amount_of_waiting = 5
-
-            for i in range(amount_of_tries):
-                try:
-                    time.sleep(amount_of_waiting)
-                    print(f"{Colors.yellow}{i}. Attempting to reconnect..{Colors.reset}")
-                    rpc = pypresence.Presence(args.client_id)
-                    rpc.connect()
-                    print(
-                        f"{Colors.green}Successfully reconnected.. Proceeding as normal.{Colors.reset}"
-                    )
-                    break
-
-                except Exception:  # pylint:disable=broad-exception-caught
-                    print(
-                        f"{Colors.red}Exception caught but continuing..{Colors.reset}"
-                    )
-            else:
-                print(
-                    f"{Colors.red}Was unable to reconnect to discord. after trying for {amount_of_tries * amount_of_waiting} seconds.{Colors.reset}"
-                )
-                sys.exit()
+            discord_reconnect_attempt(rpc, amount_of_tries=12, amount_of_waiting=5)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Script with Discord RPC.")
+    parser.add_argument(
+        "--client-id",
+        type=str,
+        default=DEFAULT_CLIENT_ID,
+        help=f"Client ID for Discord RPC. Default is {DEFAULT_CLIENT_ID}. which will show 'League of Linux' on discord",
+    )
+    parser.add_argument(
+        "--no-stats",
+        action="store_true",
+        help="use '--no-stats' to Opt out of showing in-game stats (KDA, minions) in Discord RPC",
+    )
+    parser.add_argument(
+        "--add-process",
+        nargs="+",
+        default=[],
+        help="Add custom Discord process names to the search list.",
+    )
+
+    args = parser.parse_args()
+
+    # Prints the League RPC logo
+    print(Colors().logo)
+
+    if args.no_stats:
+        print(
+            f"{Colors.green}Argument {Colors.blue}--no-stats{Colors.green} detected.. Will {Colors.red}not {Colors.green}show InGame stats{Colors.reset}"
+        )
+    if args.add_process:
+        print(
+            f"{Colors.green}Argument {Colors.blue}--add-process{Colors.green} detected.. Will add {Colors.blue}{args.add_process}{Colors.green} to the list of Discord processes to look for.{Colors.reset}"
+        )
+    if args.client_id != DEFAULT_CLIENT_ID:
+        print(
+            f"{Colors.green}Argument {Colors.blue}--client-id{Colors.green} detected.. Will try to connect by using {Colors.blue}({args.client_id}){Colors.reset}"
+        )
+
+    main(args)
