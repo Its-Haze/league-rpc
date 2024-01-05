f8a0ed82b0795cf6f840df871fa201400601afb0
Haze <haze.gpg@proton.me>
2024-01-05 21:31:40 +0100

    refactor and extract methods in gather_base_data

--- league_rpc_linux/lcu_api/base_data.py
+++ league_rpc_linux/lcu_api/base_data.py
@@ -17,50 +17,14 @@ from league_rpc_linux.models.module_data import ModuleData
 async def gather_base_data(connection: Connection, module_data: ModuleData):
     print("Gathering base data.")
     data = module_data.client_data
-    summoner_data_raw = await connection.request(
-        "GET", "/lol-summoner/v1/current-summoner"
-    )
-    summoner_data = await summoner_data_raw.json()
-
-    data.summoner_name = summoner_data[Summoner.DISPLAY_NAME]
-    data.summoner_level = summoner_data[Summoner.SUMMONER_LEVEL]
-    data.summoner_id = summoner_data[Summoner.SUMMONER_ID]
-    data.summoner_icon = summoner_data[Summoner.PROFILE_ICON_ID]
+    await gather_summoner_data(connection, data)
 
     # get Online/Away status
-    chat_data_raw = await connection.request("GET", "/lol-chat/v1/me")
-    chat_data = await chat_data_raw.json()
+    await gather_chat_status_data(connection, data)
 
-    match chat_data[LolChatUser.AVAILABILITY]:
-        case LolChatUser.CHAT:
-            data.availability = LolChatUser.ONLINE.capitalize()
-        case LolChatUser.AWAY:
-            data.availability = LolChatUser.AWAY.capitalize()
-        case _:
-            ...
+    await gather_ranked_data(connection, data)
 
-    ranked_data_raw = await connection.request(
-        "GET", "/lol-ranked/v1/current-ranked-stats/"
-    )
-    ranked_data = await ranked_data_raw.json()
-
-    data.summoner_rank = RankedStats.from_map(
-        obj_map=ranked_data,
-        ranked_type="RANKED_SOLO_5x5",
-    )
-    data.summoner_rank_flex = RankedStats.from_map(
-        obj_map=ranked_data,
-        ranked_type="RANKED_FLEX_SR",
-    )
-
-    data.arena_rank = ArenaStats.from_map(obj_map=ranked_data)
-    data.tft_rank = TFTStats.from_map(obj_map=ranked_data)
-
-    game_flow_data_raw = await connection.request(
-        "GET", "/lol-gameflow/v1/gameflow-phase"
-    )
-    game_flow_data = await game_flow_data_raw.json()
-    data.gameflow_phase = game_flow_data
+    await gather_gameflow_data(connection, data)
 
     if data.gameflow_phase == GameFlowPhase.IN_PROGRESS:
         # In Game
@@ -70,6 +34,38 @@ async def gather_base_data(connection: Connection, module_data: ModuleData):
         # In Client
         return
 
+    await gather_lobby_data(connection, data)
+
+    if data.queue_id == -1:
+        # custom game / practice tool / tutorial lobby
+        data.gamemode = "PRACTICETOOL"
+        data.map_id = 11
+        if data.is_practice:
+            data.queue = "Practice Tool"
+            data.max_players = 1
+        else:
+            data.queue = "Custom Game"
+
+        return
+
+    await gather_queue_data(connection, data)
+
+
+async def gather_queue_data(connection, data):
+    lobby_queue_info_raw = await connection.request(
+        "GET", "/lol-game-queues/v1/queues/" + str(data.queue_id)
+    )
+    lobby_queue_info = await lobby_queue_info_raw.json()
+    data.queue = lobby_queue_info[LolGameQueuesQueue.NAME]
+    data.max_players = int(
+        lobby_queue_info[LolGameQueuesQueue.MAXIMUM_PARTICIPANT_LIST_SIZE]
+    )
+    data.map_id = lobby_queue_info[LolGameQueuesQueue.MAP_ID]
+    data.gamemode = lobby_queue_info[LolGameQueuesQueue.GAME_MODE]
+    data.queue_is_ranked = lobby_queue_info[LolGameQueuesQueue.IS_RANKED]
+
+
+async def gather_lobby_data(connection, data):
     lobby_raw_data = await connection.request(
         "GET", "/lol-gameflow/v1/gameflow-metadata/player-status"
     )
@@ -93,26 +89,54 @@ async def gather_base_data(connection: Connection, module_data: ModuleData):
         LolGameflowLobbyStatus.IS_CUSTOM
     ]
 
-    if data.queue_id == -1:
-        # custom game / practice tool / tutorial lobby
-        data.gamemode = "PRACTICETOOL"
-        data.map_id = 11
-        if data.is_practice:
-            data.queue = "Practice Tool"
-            data.max_players = 1
-        else:
-            data.queue = "Custom Game"
 
-        return
+async def gather_gameflow_data(connection, data):
+    game_flow_data_raw = await connection.request(
+        "GET", "/lol-gameflow/v1/gameflow-phase"
+    )
+    game_flow_data = await game_flow_data_raw.json()
+    data.gameflow_phase = game_flow_data
 
-    lobby_queue_info_raw = await connection.request(
-        "GET", "/lol-game-queues/v1/queues/" + str(data.queue_id)
+
+async def gather_ranked_data(connection, data):
+    ranked_data_raw = await connection.request(
+        "GET", "/lol-ranked/v1/current-ranked-stats/"
     )
-    lobby_queue_info = await lobby_queue_info_raw.json()
-    data.queue = lobby_queue_info[LolGameQueuesQueue.NAME]
-    data.max_players = int(
-        lobby_queue_info[LolGameQueuesQueue.MAXIMUM_PARTICIPANT_LIST_SIZE]
+    ranked_data = await ranked_data_raw.json()
+
+    data.summoner_rank = RankedStats.from_map(
+        obj_map=ranked_data,
+        ranked_type="RANKED_SOLO_5x5",
     )
-    data.map_id = lobby_queue_info[LolGameQueuesQueue.MAP_ID]
-    data.gamemode = lobby_queue_info[LolGameQueuesQueue.GAME_MODE]
-    data.queue_is_ranked = lobby_queue_info[LolGameQueuesQueue.IS_RANKED]
+    data.summoner_rank_flex = RankedStats.from_map(
+        obj_map=ranked_data,
+        ranked_type="RANKED_FLEX_SR",
+    )
+
+    data.arena_rank = ArenaStats.from_map(obj_map=ranked_data)
+    data.tft_rank = TFTStats.from_map(obj_map=ranked_data)
+
+
+async def gather_chat_status_data(connection, data):
+    chat_data_raw = await connection.request("GET", "/lol-chat/v1/me")
+    chat_data = await chat_data_raw.json()
+
+    match chat_data[LolChatUser.AVAILABILITY]:
+        case LolChatUser.CHAT:
+            data.availability = LolChatUser.ONLINE.capitalize()
+        case LolChatUser.AWAY:
+            data.availability = LolChatUser.AWAY.capitalize()
+        case _:
+            ...
+
+
+async def gather_summoner_data(connection, data):
+    summoner_data_raw = await connection.request(
+        "GET", "/lol-summoner/v1/current-summoner"
+    )
+    summoner_data = await summoner_data_raw.json()
+
+    data.summoner_name = summoner_data[Summoner.DISPLAY_NAME]
+    data.summoner_level = summoner_data[Summoner.SUMMONER_LEVEL]
+    data.summoner_id = summoner_data[Summoner.SUMMONER_ID]
+    data.summoner_icon = summoner_data[Summoner.PROFILE_ICON_ID]
